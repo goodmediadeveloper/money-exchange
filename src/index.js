3f654a4ef1c668e111827918ac0965b100823474
Dzmitry Prokharau <goodmediadeveloper@gmail.com>
2019-02-28 14:03:29 +0300

    com-1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,60 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var tmp = 0;
+var sum = {};
+
+if (currency > 10000) {
+    return sum = {"error": "You are rich, my friend! We don't have so much coins for exchange"};
+}else if (currency < 1) {
+    return sum = {};
+}else {
+
+            if (currency>=50) {
+                while (currency>=50) {
+                    currency = currency - 50;
+                    tmp++;
+                }
+                sum['H'] = tmp;
+                tmp = 0;
+            }
+
+            if (currency>=25) {
+                while (currency>=25) {
+                    currency = currency - 25;
+                    tmp++;
+                }
+                sum['Q'] = tmp;
+                tmp = 0;
+            }
+
+            if (currency>=10) {
+                while (currency>=10) {
+                    currency = currency - 10;
+                    tmp++;
+                }
+                sum['D'] = tmp;
+                tmp = 0;
+            }
+
+            if (currency>=5) {
+                while (currency>=5) {
+                    currency = currency - 5;
+                    tmp++;
+                }
+                sum['N'] = tmp;
+                tmp = 0;
+            }
+
+            if (currency>=1) {
+                while (currency>=1) {
+                    currency = currency - 1;
+                    tmp++;
+                }
+                sum['P'] = tmp;
+                tmp = 0;
+            }
+            return sum;
 }
+
+}
+
